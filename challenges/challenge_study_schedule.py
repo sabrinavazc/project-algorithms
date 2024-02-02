4c0ee5cd69e6ba6a619bc4b3be1f81760909e52c
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-02-02 19:28:38 -0300

    requeriment_01_innitial_commit

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,21 @@
+from collections import Counter
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # validações
+    if any(end < start for start, end in permanence_period):
+        raise ValueError("Dados de permanência do usuário inválidos.")
+
+    if target_time is None:
+        raise ValueError("Insira um target time válido!")
+
+    # algoritmo de busca
+    permanence_result = []
+
+    for i1, i2 in permanence_period:
+        sequency = range(i1, i2 + 1)
+        permanence_result.extend(sequency)
+
+    counter = Counter(permanence_result)
+
+    return counter[target_time]
