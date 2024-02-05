52c7164d78ff5970af68081bd80df6f0e594e0ea
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-02-05 19:32:42 -0300

    requeriment_05_WIP

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,19 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not nums
+        or not isinstance(nums, list)
+        or isinstance(nums, str)
+        or len(nums) < 2
+    ):
+        return False
+
+    if any(num < 1 for num in nums):
+        return False
+
+    nums.sort()
+
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+
+    return False
