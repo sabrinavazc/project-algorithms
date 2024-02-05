c049ceb81d70df62e4751735e9b95cdc758cff0b
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-02-05 18:07:55 -0300

    requeriment_03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    if low_index >= high_index:
+        return True
+    else:
+        return (
+            word[low_index] == word[high_index]
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1)
+            )
+
+
+print(is_palindrome_recursive("ana", 0, len("ana") - 1))  # True
