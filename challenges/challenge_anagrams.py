dddd133d0f8aec5ed6111712b7306bca53bb3d5e
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-02-05 19:14:20 -0300

    fix: requeriment_04_and_requeriment_06_finished

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,32 +1,13 @@
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
+def sort_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-        merge_sort(left_half)
-        merge_sort(right_half)
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
 
-        i, j, k = 0, 0, 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
+    return sort_sort(left) + middle + sort_sort(right)
 
 
 def is_anagram(first_string, second_string):
@@ -36,8 +17,8 @@ def is_anagram(first_string, second_string):
     first_word = list(first_string.lower())
     second_word = list(second_string.lower())
 
-    merge_sort(first_word)
-    merge_sort(second_word)
+    first_word[:] = sort_sort(first_word)
+    second_word[:] = sort_sort(second_word)
 
     sorted_first = "".join(first_word)
     sorted_second = "".join(second_word)
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    low_index, high_index = 0, len(word) - 1
+
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+
+    return True
